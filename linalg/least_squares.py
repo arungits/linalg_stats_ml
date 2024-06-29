82f54d078e11ca151a029af3217b61c7880f6e7d
arungits <mails4arun82@gmail.com>
2024-06-29 14:12:21 +0530

    Least squares method

---
+++ linalg/least_squares.py
@@ -0,0 +1,32 @@
+import numpy as np
+import numpy.linalg as LA
+
+def least_squares_fit_by_projection(A, b):
+    # Use the following matrix to project b onto the column space of A
+    # p = A@inv(A_transpose@A)@A_transpose@b
+    # Then solve for x using the equation Ax = p using pseudoinverse of A
+    # where x is the best fit solution for Ax = b
+    A_transpose = A.transpose()
+    p = A @ LA.inv(A_transpose @ A) @ A_transpose @ b
+    x = LA.pinv(A) @ p
+    return x
+
+def least_squares_fit_by_direct_formula(A, b):
+    # This method is similar to least_squares_fit_by_projection
+    # Here instead of solving Ax = p by elimination, we directly compute x as
+    # dot product of inv(A_transpose@A)@A_transpose@b
+    A_transpose = A.transpose()
+    x = LA.inv(A_transpose @ A) @ A_transpose @ b
+    return x
+
+
+# Test code
+A = np.random.randn(10,2)
+b = np.random.randn(10)
+x_projection = np.round(least_squares_fit_by_projection(A, b),4)
+x_direct = np.round(least_squares_fit_by_direct_formula(A, b), 4)
+# Ensure both methods return the same x
+assert(np.all(x_projection == x_direct))
+
+
+
